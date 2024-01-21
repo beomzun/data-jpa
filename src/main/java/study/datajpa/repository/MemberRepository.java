6d3ad9e7165b855ac02774d3c8ecaaea4aec9dcc
beomzun <punpon53@naver.com>
2024-01-21 16:57:32 +0900

    feat: NamedQuery 추가

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package study.datajpa.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -9,4 +11,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     List<Member> findTop3HelloBy();
+
+//    @Query(name = "Member.findByUsername")    //없어도 실행이 된다. Entity에서 메서드명과 같은 namedQuery를 우선적으로 찾음.
+//    없으면 메서드명에 따라서 쿼리 생성
+    List<Member> findByUsername(@Param("username") String username);
 }
